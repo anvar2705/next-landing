1ddb398400f21d96419c7e7990cc28d81e36a28e
Appazov <anvar978@yandex.ru>
2023-03-11 15:31:40 +0500

    Add eslint, stylelint, sass

---
+++ src/shared/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,9 @@
+import styles from './TestComponent.module.scss';
+
+const TestComponent = (): JSX.Element => (
+  <div className={styles.root}>
+    TestComponent
+  </div>
+);
+
+export default TestComponent;
